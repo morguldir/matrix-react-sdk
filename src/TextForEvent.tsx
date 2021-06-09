137e12f256e4a08646a9d59fad760e2ecc884934
Tulir Asokan <tulir@maunium.net>
2021-06-09 13:51:13 +0300

    Revert "Merge pull request 5316 from matrix-org/travis/less-acl-noise"
    This reverts commit 5c79f35cc5b2aceb2da3028b30864b873f35a8d1, reversing
    changes made to 9234a58bdfdfa7a81003d9773e683d9c35adff42.

--- src/TextForEvent.tsx
+++ src/TextForEvent.tsx
@@ -346,6 +346,7 @@ function textForGuestAccessEvent(ev: MatrixEvent): (() => string) | null {
 function textForServerACLEvent(ev: MatrixEvent): (() => string) | null {
     const senderDisplayName = ev.sender && ev.sender.name ? ev.sender.name : ev.getSender();
     const prevContent = ev.getPrevContent();
+    const changes = [];
     const current = ev.getContent();
     const prev = {
         deny: Array.isArray(prevContent.deny) ? prevContent.deny : [],
@@ -353,23 +354,54 @@ function textForServerACLEvent(ev: MatrixEvent): (() => string) | null {
         allow_ip_literals: prevContent.allow_ip_literals !== false,
     };
 
-    let getText: () => string;
+    let text = "";
     if (prev.deny.length === 0 && prev.allow.length === 0) {
-        getText = () => _t("timeline|m.room.server_acl|set", { senderDisplayName });
+        text = `${senderDisplayName} set server ACLs for this room: `;
     } else {
-        getText = () => _t("timeline|m.room.server_acl|changed", { senderDisplayName });
+        text = `${senderDisplayName} changed the server ACLs for this room: `;
     }
 
     if (!Array.isArray(current.allow)) {
         current.allow = [];
     }
-
-    // If we know for sure everyone is banned, mark the room as obliterated
+    /* If we know for sure everyone is banned, don't bother showing the diff view */
     if (current.allow.length === 0) {
-        return () => getText() + " " + _t("timeline|m.room.server_acl|all_servers_banned");
+        text += "🎉 All servers are banned from participating! This room can no longer be used.";
+        return () => text;
     }
 
-    return getText;
+    if (!Array.isArray(current.deny)) {
+        current.deny = [];
+    }
+
+    const bannedServers = current.deny.filter((srv) => typeof srv === "string" && !prev.deny.includes(srv));
+    const unbannedServers = prev.deny.filter((srv) => typeof srv === "string" && !current.deny.includes(srv));
+    const allowedServers = current.allow.filter((srv) => typeof srv === "string" && !prev.allow.includes(srv));
+    const unallowedServers = prev.allow.filter((srv) => typeof srv === "string" && !current.allow.includes(srv));
+
+    if (bannedServers.length > 0) {
+        changes.push(`Servers matching ${bannedServers.join(", ")} are now banned.`);
+    }
+
+    if (unbannedServers.length > 0) {
+        changes.push(`Servers matching ${unbannedServers.join(", ")} were removed from the ban list.`);
+    }
+
+    if (allowedServers.length > 0) {
+        changes.push(`Servers matching ${allowedServers.join(", ")} are now allowed.`);
+    }
+
+    if (unallowedServers.length > 0) {
+        changes.push(`Servers matching ${unallowedServers.join(", ")} were removed from the allowed list.`);
+    }
+
+    if (prev.allow_ip_literals !== current.allow_ip_literals) {
+        const allowban = current.allow_ip_literals ? "allowed" : "banned";
+        changes.push(`Participating from a server using an IP literal hostname is now ${allowban}.`);
+    }
+
+    text += changes.join(" ");
+    return () => text;
 }
 
 function textForMessageEvent(ev: MatrixEvent, client: MatrixClient): (() => string) | null {
